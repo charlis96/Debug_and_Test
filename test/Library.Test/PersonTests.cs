804579489f824bc6aa5615d9f9725c5b8915caf7
charlis96 <ciarlohernan@gmail.com>
2022-09-07 10:15:03 -0300

    con test

--- test/Library.Test/PersonTests.cs
+++ test/Library.Test/PersonTests.cs
@@ -13,9 +13,31 @@ namespace Tests
         }
 
         [Test]
-        public void Test1() // Cambiá el nombre para indicar qué estás probando
+        public void CreatePersonWithNameAndIdTest() // Cambiá el nombre para indicar qué estás probando
         {
-            // Insertá tu código  de pruebaaquí
+            // Configuración
+            const string expectedID = "12345672";
+            const string expectedName = "Test Name";
+            const string idOK = "12345672";
+            // Ejecución
+            Person person = new Person(expectedName, idOK);
+            // Comprobación
+            Assert.AreEqual(expectedID, person.ID);
+            Assert.AreEqual(expectedName, person.Name);
+        }
+
+        [Test]
+        public void CreatePersonWithNameAndIncorrectIdTest() // Cambiá el nombre para indicar qué estás probando
+        {
+            // Configuración
+            const string expectedID = null;
+            const string expectedName = "Test Name";
+            const string wrongId = "4.753.174-6";
+            // Ejecución
+            Person person = new Person(expectedName, wrongId);
+            // Comprobación
+            Assert.AreEqual(expectedID, person.ID);
+            Assert.AreEqual(expectedName, person.Name);
         }
     }
 }
